bb944c27cfdb303b04d4bd7e3e0c56519687b3ef
Daniel <daniel.jalonso07@gmail.com>
2025-11-20 20:27:58 +0100

    start

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import dominio.CPU;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Me creo el primer procesador en blanco
+        CPU cpu = new CPU();
 
+        // Asigno los valores del primer procesador
+        cpu.setTipo("AMD");
+        cpu.setGeneracion("7");
+        cpu.setNucleo("i7");
+        cpu.setMemCache("2Mb");
+        cpu.setVelocidad("3.6Ghz");
+
+        // Me creo otro procesador en blanco
+        CPU cpu2 = new CPU();
+
+        // Asigno los valores del segundo procesador
+        cpu2.setTipo("Intel");
+        cpu2.setGeneracion("13");
+        cpu2.setNucleo("i5");
+        cpu2.setMemCache("1Mb");
+        cpu2.setVelocidad("1.8Ghz");
+
+        // Imprimo cada valor del primer procesador
+        System.out.println(cpu.getTipo());
+        System.out.println(cpu.getGeneracion());
+        System.out.println(cpu.getNucleo());
+        System.out.println(cpu.getMemCache());
+        System.out.println(cpu.getVelocidad());
 
     }
 }
--- src/dominio/CPU.java
+++ src/dominio/CPU.java
@@ -8,4 +8,43 @@ public class CPU {
     private String generacion;
     private String nucleo;
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(String velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getMemCache() {
+        return memCache;
+    }
+
+    public void setMemCache(String memCache) {
+        this.memCache = memCache;
+    }
+
+    public String getGeneracion() {
+        return generacion;
+    }
+
+    public void setGeneracion(String generacion) {
+        this.generacion = generacion;
+    }
+
+    public String getNucleo() {
+        return nucleo;
+    }
+
+    public void setNucleo(String nucleo) {
+        this.nucleo = nucleo;
+    }
 }
