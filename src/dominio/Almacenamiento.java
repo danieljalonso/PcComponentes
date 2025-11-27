2315766a418037045726d58606e4f6e69d502d4e
danieljalonso <daniel.jalonso@gmail.com>
2025-11-27 19:53:39 +0100

    Añadidos los valores de cada entidad y mostradas por pantalla

--- src/dominio/Almacenamiento.java
+++ src/dominio/Almacenamiento.java
@@ -6,4 +6,27 @@ public class Almacenamiento {
     private String capacidad;
     private String velocidad;
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(String velocidad) {
+        this.velocidad = velocidad;
+    }
 }
